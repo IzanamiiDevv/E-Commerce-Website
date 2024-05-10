7963f0ff04e22018da1737c2b5d15276398a241d
IzanamiiDevv <rafaeloli596@gmail.com>
2024-05-10 18:17:54 +0800

    add item

---
+++ js/home.js
@@ -0,0 +1,21 @@
+function addItem({img,name,price}){
+    const temp = `
+<div class="box">
+    <a href="" class="fas fa-eye"></a>
+    <div class="price">${price}</div>
+    <img src="${img}" class="image">
+    <div class="name">${name}</div>
+    <input type="number" name="product_quantity" value="1" min="0" class="qty">
+    <input type="submit" value="add to wishlist" name="add_to_wishlist" class="option-btn">
+    <input type="submit" value="add to cart" name="add_to_cart" class="btn">
+</div>
+`
+
+    document.getElementById('item-container').innerHTML += temp;
+}
+
+addItem({
+    img:'flowers/beach florist.jpg',
+    name:'test',
+    price:'$10',
+});
---
+++ js/items.js
@@ -0,0 +1,62 @@
+const items = [
+    {
+        name:'beach florist',
+        img:'flowers/beach florist.jpg',
+        price:''
+    },
+    {
+        name:'bloomnation',
+        img:'flowers/bloomnation.jpg',
+        price:''
+    },
+    {
+        name:'cottage rose',
+        img:'flowers/cottage rose.jpg',
+        price:''
+    },
+    {
+        name:'fun bloomnation',
+        img:'flowers/fun bloomnation.jpg',
+        price:''
+    },
+    {
+        name:'lavendor rose',
+        img:'flowers/lavendor rose.jpg',
+        price:''
+    },
+    {
+        name:'pink bouquet',
+        img:'flowers/pink bouquet.jpg',
+        price:''
+    },
+    {
+        name:'pink queen rose',
+        img:'flowers/pink queen rose',
+        price:''
+    },
+    {
+        name:'pink queen rose',
+        img:'flowers/pink queen rose.jpg',
+        price:''
+    },
+    {
+        name:'red tulipa',
+        img:'flowers/red tulipa.jpg',
+        price:''
+    },
+    {
+        name:'white bouquet',
+        img:'flowers/white bouquet.jpg',
+        price:''
+    },
+    {
+        name:'yellow queen rose',
+        img:'flowers/yellow queen rose.jpg',
+        price:''
+    },
+    {
+        name:'yellow tulipa',
+        img:'flowers/yellow tulipa.jpg',
+        price:''
+    }
+];
